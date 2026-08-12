49b0e5f2e1d91755bbf3097721d4010af5b91dfa
alsi-lawr <177320313+alsi-lawr@users.noreply.github.com>
2026-08-12 09:33:46 +0100

    chore: prepare v0.4.1

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
         in
         pkgs.buildDotnetModule {
           pname = "dotnet-workspace-explorer";
-          version = "0.4.0";
+          version = "0.4.1";
 
           src = lib.fileset.toSource {
             root = ./.;
